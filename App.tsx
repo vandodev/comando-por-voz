b348b998c7603bfda50ed52c60ed9a23933a606e
vandodev <vandoaparecido@hotmail.com>
2024-01-24 21:45:32 -0300

    :speaker: :microphone: :hear_no_evil: layout base

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, StyleSheet, TextInput, Pressable} from 'react-native'
+import {Feather} from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" backgroundColor="transparent" translucent />
+
+      <View style={styles.header}>
+        <TextInput style={styles.input} placeholder='Pesquisar ...' />
+
+        <Pressable style={styles.button}>
+          <Feather name='mic' color="#fff" size={24}/>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 32,
+    paddingVertical: 52,
   },
+
+  header:{
+    width: '100%',
+    flexDirection: "row",
+    alignItems: "center",
+    gap:12,
+  },
+
+  input:{
+    flex: 1,
+    height: 54,
+    padding: 16,
+    fontSize: 16,
+    borderRadius: 12,
+    backgroundColor:"#D9E6Eb"
+  },
+
+  button:{
+    height: 54,
+    width: 54,
+    borderRadius: 12,
+    backgroundColor: "#6F4AE5",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
